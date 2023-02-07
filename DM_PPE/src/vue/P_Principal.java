5cea4d09b30ffb53e2ebcaee22827cebad0825b5
LePetitMoka <smokemat@orange.fr>
2023-02-07 12:13:46 +0100

    commit ?

--- DM_PPE/src/vue/P_Principal.java
+++ DM_PPE/src/vue/P_Principal.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 
 import javax.swing.JPanel;
 
-public class P_Principal extends JPanel {
+public abstract class P_Principal extends JPanel {
 	public P_Principal(Color uneCouleur) {
 		this.setBounds(30, 80, 830, 300);
 		this.setBackground(uneCouleur);
